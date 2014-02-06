ba4538401e88894bd1622cab03b2db50b1aaa7e1
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-05 16:56:22 -0800

    Update package configuration

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     name='tangled.sqlalchemy',
     version='0.1.dev0',
     description='Tangled SQLAlchemy integration',
+    long_description=open('README.rst').read(),
+    url='http://tangledframework.org/',
+    author='Wyatt Baldwin',
+    author_email='self@wyattbaldwin.com',
     packages=find_packages(),
     install_requires=(
         'tangled>=0.1.dev0',
