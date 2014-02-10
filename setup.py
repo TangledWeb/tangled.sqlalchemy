8a8a4179c6a641d9e3dad23e47fada3528c26905
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-09 17:25:17 -0800

    Back to development: 0.1a3.dev0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tangled.sqlalchemy',
-    version='0.1a2',
+    version='0.1a3.dev0',
     description='Tangled SQLAlchemy integration',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
